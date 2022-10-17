bbca99d1e94f503288b4f522e0490abce94fd13c
Kunal Patel <kpate127@asu.edu>
2022-10-17 15:51:23 -0700

    first commit

---
+++ BuyerSellerSystem/src/Facade.java
@@ -0,0 +1,72 @@
+public class Facade {
+
+	private int UserType;
+
+	private Product theSelectedProduct;
+
+	private int nProductCategory;
+
+	private ClassProductList theProductList;
+
+	private Person thePerson;
+
+	public boolean login() {
+		return false;
+	}
+
+	public void addTrading() {
+
+	}
+
+	public void viewTrading() {
+
+	}
+
+	public void decideBidding() {
+
+	}
+
+	public void discussBidding() {
+
+	}
+
+	public void submitBidding() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void remind() {
+
+	}
+
+	public void operation7() {
+
+	}
+
+	public void operation8() {
+
+	}
+
+	public void operation9() {
+
+	}
+
+	public void createUser(UserInfoItem userinfoitem) {
+
+	}
+
+	public void AttachProductTo User() {
+
+	}
+
+	public Product SelectProduct() {
+		return null;
+	}
+
+	public void productOperation() {
+
+	}
+
+}
---
+++ BuyerSellerSystem/src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+	private ProductMenu theProductMenu;
+
+	private ProductMenu productMenu;
+
+	public void showMenu() {
+
+	}
+
+	public void shoeAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public ProductMenu CreateProductMenu() {
+		return null;
+	}
+
+}
